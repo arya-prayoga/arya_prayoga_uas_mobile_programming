fbaccdd9fc5082bb25ef44ddbb3f6990f8a37e3a
arya-prayoga <aryaprayoga444@gmail.com>
2022-07-01 18:24:10 +0700

    Edit UI Login Page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,78 +11,179 @@ class LoginPage extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Image.asset('assets/images/back_arrow.png'),
-            ),
-            Image.asset('assets/images/logo.png'),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Username',
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  // side: BorderSide(color: Colors.black),
+                  fixedSize: Size(50, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    side: BorderSide(color: Colors.grey),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Image.asset('assets/images/back_arrow.png'),
               ),
             ),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Password',
-                suffixIcon: Icon(Icons.visibility),
+            SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width / 2,
+              child: Image.asset('assets/images/logo.png'),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Username',
+                ),
               ),
             ),
-            Text('Forgot Password?'),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Login'),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Password',
+                  suffixIcon: Icon(Icons.visibility),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Text('Forgot Password?'),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                  minimumSize: Size.fromHeight(50),
+                  side: BorderSide(color: Colors.black),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 15,
             ),
             Row(
               children: <Widget>[
                 Expanded(
-                  child: Divider(
-                    color: Colors.black,
-                    thickness: 1,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Divider(
+                      color: Colors.black,
+                      thickness: 1,
+                    ),
                   ),
                 ),
                 Text('Or Login with'),
                 Expanded(
-                  child: Divider(
-                    color: Colors.black,
-                    thickness: 1,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Divider(
+                      color: Colors.black,
+                      thickness: 1,
+                    ),
                   ),
                 ),
               ],
             ),
+            SizedBox(
+              height: 15,
+            ),
             Row(
               children: <Widget>[
                 Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset('assets/images/facebook_logo.png'),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        minimumSize: const Size.fromHeight(50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          side: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Image.asset('assets/images/facebook_logo.png'),
+                    ),
                   ),
                 ),
                 Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset('assets/images/google_logo.png'),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        minimumSize: const Size.fromHeight(50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          side: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Image.asset('assets/images/google_logo.png'),
+                    ),
                   ),
                 ),
                 Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset('assets/images/apple_logo.png'),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        minimumSize: const Size.fromHeight(50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          side: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Image.asset('assets/images/apple_logo.png'),
+                    ),
                   ),
                 ),
               ],
             ),
             Spacer(),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text("Don't have an account?"),
                 TextButton(
                   onPressed: () {},
-                  child: Text('Register Now'),
+                  child: Text(
+                    'Register Now',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 5, 255, 205),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
                 ),
               ],
             ),
